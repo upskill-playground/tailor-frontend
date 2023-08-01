eb44a5ddb14ada801cd256be4e97ff249a7f0a55
Roland Enola <aloneroland@gmail.com>
2023-08-01 23:39:33 +0100

    intial setup

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      orange: {
+        900: "#7E2410",
+        800: "#9C2A10",
+        700: "#C4320A",
+        600: "#EC4A0A",
+        500: "#FB6514",
+        400: "#FD853A",
+        300: "#FEB273",
+        200: "#FDDCAB",
+        100: "#FFEAD5",
+        50: "#FFF6ED",
+        25: "#FFFAF5",
+      },
+      gray: {
+        900: "#101828",
+        800: "#1D2939",
+        700: "#344054",
+        600: "#475467",
+        500: "#667085",
+        400: "#98A2B3",
+        300: "#D0D5DD",
+        200: "#EAECF0",
+        100: "#F2F4F7",
+        50: "#F9FAFB",
+        25: "#FCFCFD",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
